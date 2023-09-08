0586a08321ce9014bee03a5859784784357ad171
Mikey Streed <mbstreed19@outlook.com>
2023-09-07 23:11:06 -0500

    rainbow delimiters

--- config.el
+++ config.el
@@ -418,9 +418,37 @@
 (electric-indent-mode -1)
 (setq org-edit-src-content-indentation 0)
 
+(require 'org-tempo)
+
+(use-package perspective
+  :custom
+  ;; NOTE! I have also set 'SCP =' to open the perspective menu.
+  ;; I'm only setting the additional binding because setting it
+  ;; helps suppress an annoying warning message.
+  (persp-mode-prefix-key (kbd "C-c M-p"))
+  :init 
+  (persp-mode)
+  :config
+  ;; Sets a file to write to when we save states
+  (setq persp-state-default-file "~/.config/emacs/sessions"))
+
+;; This will group buffers by persp-name in ibuffer.
+(add-hook 'ibuffer-hook
+          (lambda ()
+            (persp-ibuffer-set-filter-groups)
+            (unless (eq ibuffer-sorting-mode 'alphabetic)
+              (ibuffer-do-sort-by-alphabetic))))
+
+;; Automatically save perspective states to file when Emacs exits.
+(add-hook 'kill-emacs-hook #'persp-state-save)
+
 (use-package projectile
   :config
   (projectile-mode 1))
+
+(use-package rainbow-delimiters
+  :hook ((emacs-lisp-mode . rainbow-delimiters-mode)
+         (clojure-mode . rainbow-delimiters-mode)))
 
 (use-package rainbow-mode
   :diminish
